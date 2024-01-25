8ca3d7e94a5734f19b2802814ece8921203ff22e
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2024-01-24 22:18:52 -0300

    add: função de criptografar e descriptografar

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+const caracteresCriptografia = {
+  e: "enter",
+  i: "imes",
+  a: "ai",
+  u: "ufat",
+  o: "ober",
+};
+
+function getMensagem() {
+  const mensagem = document.getElementById("mensagem").value;
+  return mensagem;
+}
+
+function showMensagem(mensagem) {
+  const mensagemTrabalhada = document.getElementById("mensagemTrabalhada");
+  mensagemTrabalhada.innerText = mensagem;
+}
+
+function descriptografarMensagem() {
+  let mensagem = getMensagem();
+
+  for (const key in caracteresCriptografia) {
+    mensagem = mensagem.replaceAll(caracteresCriptografia[key], key);
+  }
+
+  showMensagem(mensagem);
+}
+
+function criptografarMensagem() {
+  let mensagem = getMensagem();
+
+  for (const key in caracteresCriptografia) {
+    mensagem = mensagem.replaceAll(key, caracteresCriptografia[key]);
+  }
+
+  showMensagem(mensagem);
+}
