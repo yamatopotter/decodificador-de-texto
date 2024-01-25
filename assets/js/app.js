75ced16f1fb820e7c2ab0a7fe9eb694d47edc9b0
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2024-01-24 22:55:12 -0300

    add: função de copiar na interface

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,14 @@ function getMensagem() {
 
 function showMensagem(mensagem) {
   const mensagemTrabalhada = document.getElementById("mensagemTrabalhada");
+  const mostrarMensagemTrabalhada = document.getElementById(
+    "mostrarMensagemTrabalhada"
+  );
+  const nadaEncontrado = document.getElementById("nadaEncontrado");
+
+  nadaEncontrado.classList.add("hide");
+  mostrarMensagemTrabalhada.classList.remove("hide");
+
   mensagemTrabalhada.innerText = mensagem;
 }
 
@@ -35,6 +43,18 @@ function criptografarMensagem() {
 
   showMensagem(mensagem);
 }
+
+// codigo retirado do W3Schools
+function copiarMensagem() {
+  // Get the text field
+  let mensagemTrabalhada = document.getElementById("mensagemTrabalhada");
+
+  // Copia o texto para area de transferência
+  navigator.clipboard.writeText(mensagemTrabalhada.innerText);
+
+  // Exibe um alert para a cópia do texto
+  alert("Mensagem copiada: " + mensagemTrabalhada.innerText);
+} 
 
 function limparMensagemTrabalhada(){
     const mensagem = getMensagem();
